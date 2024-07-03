ce1ed1e9d62f61ccd1f49a5dc2ce5b7c8e0322f4
Anant1606 <anantsharma.1839@gmail.com>
2024-07-03 21:17:35 +0530

    change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -72,7 +72,7 @@ const Home = ({ ratio }) => {
           />
 
           <div>
-            <a href="mailto:official.6packprogrammer@gmail.com">Hire Me</a>
+            <a href="mailto:anantsharma.1839@gmail.com">Hire Me</a>
             <a href="#work">
               Projects <BsArrowUpRight />
             </a>
